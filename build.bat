0c5fbf7375ff6ee1899376d1b100af7afe9c067b
mothmaux <creeperondaroof@gmail.com>
2026-05-24 12:30:33 -0400

    Add go experimental to build scripts

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@
 set GOOS=windows
 set GOARCH=amd64
 set CGO_ENABLED=1
+REM uncomment to use golang experimental garbage collector. Requires golang 1.25 or higher.
+:: set GOEXPERIMENT=greenteagc
 
 go build -o OnlySats.exe main.go
 if %ERRORLEVEL% neq 0 (
